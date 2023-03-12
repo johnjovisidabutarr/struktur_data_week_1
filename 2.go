44d78b4f46f0199854a7f00bc06aaa735436c07e
johnjovisidabutarr <johnjovisidabutar@student.telkomuniversity.ac.id>
2023-03-12 21:15:12 +0700

    fixing 2.go

--- 2.go
+++ 2.go
@@ -29,8 +29,7 @@ func rataRata(T *arrNilai, n *int) float64 {
 	return nilai
 }
 
-func minimum(T *arrNilai, n *int) float64 {
-	var minValue float64
+func minMax(T *arrNilai, n *int, minValue,maxValue *float64) {
 	for i := 0; i < *n; i++ {
 		for j := i; j < *n; j++ {
 			if (*T)[j].nilai > (*T)[i].nilai {
@@ -38,33 +37,20 @@ func minimum(T *arrNilai, n *int) float64 {
 			}
 		}
 	}
-	minValue = (*T)[*n-1].nilai
-	return minValue
-}
+	*minValue = (*T)[*n-1].nilai
+	*maxValue = (*T)[0].nilai
 
-func maximum(T *arrNilai, n *int) float64 {
-	var maxValue float64
-	for i := 0; i < *n; i++ {
-		for j := i; j < *n; j++ {
-			if (*T)[j].nilai > (*T)[i].nilai {
-				(*T)[j], (*T)[i] = (*T)[i], (*T)[j]
-			}
-		}
-	}
-	maxValue = (*T)[0].nilai
-	return maxValue
 }
 
 func main() {
-	var array arrNilai
+	var T arrNilai
 	var jumlah int
 	var average, minValue, maxValue float64
 
-	inputNilai(&array, &jumlah)
-	average = (rataRata(&array, &jumlah))
-	maxValue = maximum(&array, &jumlah)
-	minValue = minimum(&array, &jumlah)
-
+	inputNilai(&T, &jumlah)
+	average = (rataRata(&T, &jumlah))
+	minMax(&T,&jumlah,&minValue,&maxValue)
+	
 	fmt.Println("Banyak nilai :", jumlah)
 	fmt.Println("Nilai rata-rata :", average)
 	fmt.Println("Nilai maximum :", maxValue)
